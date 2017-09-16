1a26b373b74bf0111fd8bcb1828ea4c036625deb
Daniel Hernandez <daniel56hdz@gmail.com>
2017-09-16 13:51:04 -0500

    add search function that validates id

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -8,11 +8,15 @@ const connection = mysql.createConnection({
     database: 'bamazonDB'
 });
 
+var allRows;
+var specificRow;
+
 const queryAll = () => {
     return new Promise((res, rej) => {
         connection.query('SELECT id, product_name, price, stock_quantity FROM products', (err, rows) => {
             if (err) throw err; 
-            res(console.log(rows));
+            allRows = rows;
+            res(console.log(allRows));
         });
     });
 }
@@ -22,14 +26,32 @@ const isInteger = (str) => {
     return str % 1 === 0; 
 }
 
+const searchArray = (arr, property, value) => {
+    arr.forEach(function(element, index) {
+        if (element[property] === value) specificRow = arr[index];
+        return specificRow;
+    }, this);
+}
+
 const questions = [{
     name: "idOfProduct",
     type: "input",
     message: "What is the ID of the product you want to buy?",
     validate: (value)=> {
         const pass = isInteger(value); // turns string to number
-        if (pass) return true;
-        return 'Please enter a valid integer';
+        if (!pass) return 'Please enter a valid ID';
+        searchArray(allRows, 'id', +value);
+        if (!specificRow) return 'Try again, ID is not in the database.'
+        return true;
+    }
+}, {
+    name: "qtyOfProduct",
+    type: "input",
+    message: "How many units of the product would you like to buy?",
+    validate: (value)=> {
+        const pass = isInteger(value);
+        if (!pass) return 'Please enter a valid amount of units';
+        return true;
     }
 }];
 
@@ -40,9 +62,12 @@ connection.connect((err)=> {
         console.log('\n');
         inquirer.prompt(questions).then((answers) => {
             console.log(answers);
+            console.log(specificRow);
         });
     });
 });
+
+
 
 
 
