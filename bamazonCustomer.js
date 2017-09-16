9340dfaa4105169a08f4ce2d612c34314398e250
Daniel Hernandez <daniel56hdz@gmail.com>
2017-09-16 15:55:15 -0500

    add comments

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,8 +15,7 @@ const queryAll = () => {
     return new Promise((res, rej) => {
         connection.query('SELECT id, product_name, price, stock_quantity FROM products', (err, rows) => {
             if (err) throw err; 
-            allRows = rows;
-            res(allRows);
+            res(allRows = rows); // resolves promise
         });
     });
 }
@@ -28,26 +27,23 @@ const queryUpdateRow = (id, quantity) => {
             res(specificRow.price * quantity);
         })
     });
-    
 }
-
 
 const isInteger = (str) => {
     str = Number(str); // Number over parseInt to reject decimals
-    return (str % 1 === 0 && str > 0); 
+    return (str % 1 === 0 && str > 0); // positive integers only
 }
 
 const searchArray = (arr, property, value) => {
     arr.forEach(function(element, index) {
         if (element[property] === value) specificRow = arr[index];
-        return specificRow;
     }, this);
 }
 
 const checkQuantity = (obj, property, value) => {
     if(obj[property] >= value) return value;
     return false;
-}
+} 
 
 const questions = [{
     name: "idOfProduct",
@@ -60,7 +56,7 @@ const questions = [{
         if (specificRow.stock_quantity === 0) return `Sorry, we don\'t have ${specificRow.product_name} in stock`;
         if (!specificRow) return 'Try again, ID is not in the database.';
         return true;
-    }
+    } // end validation
 }, {
     name: "qtyOfProduct",
     type: "input",
@@ -71,32 +67,20 @@ const questions = [{
         const quantity = checkQuantity(specificRow, 'stock_quantity', +value);
         if (!quantity) return 'Insufficient quantity';
         return true;
-    }
-}];
+    } // end validation
+}]; // end of questions
 
 connection.connect((err)=> {
     if (err) throw err;
     console.log('You are now connected...');
     queryAll().then((rows) => {
-        console.log(rows);
+        console.log(rows); // allRows
         console.log('\n');
         inquirer.prompt(questions).then((answers) => {
             queryUpdateRow(+answers.idOfProduct, +answers.qtyOfProduct).then((total)=> {
                 console.log(`Your total is ${total}`);
                 connection.end();
             });
-        });
+        }); // end of inquirer
     });
 });
-
-
-
-
-
-
-
-
-
-
-
-
